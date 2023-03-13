d183494e6455ed476a4b9f470d74b25be4f2ae8f
mark-k-mokaya <mark-k-mokaya.gmail.com>
2023-03-13 13:19:20 +0300

    Set up Redux and Fetch Products from API

---
+++ client/src/redux/slices/products.js
@@ -0,0 +1,32 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+export const initialState = {
+  loading: false, // add spinner
+  error: null,
+  products: []
+};
+
+export const productsSlice = createSlice({
+  name: "products",
+  initialState,
+  reducers: {
+    setLoading: (state) => {
+      state.loading = true;
+    }, 
+    setProducts: (state, {payload}) => {
+      state.loading = false;
+      state.error = null;
+      state.products = payload;
+    }, 
+    setError: (state, {payload}) => {
+      state.loading = false;
+      state.error = payload;
+    }
+  }
+});
+
+export const {setLoading, setProducts, setError} = productsSlice.actions;
+
+export default productsSlice.reducer;
+
+export const productsSelector = (state) => state.products;
